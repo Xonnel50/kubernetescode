11a9a47c097051649ef7629ed1853b1fec88745e
Xonnel50 <79181881+Xonnel50@users.noreply.github.com>
2022-11-05 16:14:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Just completed my frst Argocd deployment...Woooah!!!'
+    return 'Just completed my frst Argocd deployment...HELL Woooah!!!'

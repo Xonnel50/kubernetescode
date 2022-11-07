1a466e63707efb4045afc412ca377cdf0846c1f9
Xonnel50 <79181881+Xonnel50@users.noreply.github.com>
2022-11-06 20:00:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is the start of a new week.  Time to get rolling with the interviews!!!'
+    return 'This is the Day!!!'
